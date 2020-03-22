56a6b779d68c3f63af8a9da22bb402982a1d1c49
Andrew Walker <andrew.walker73@gmail.com>
2020-03-22 20:55:30 +0200

    before cloud

---
+++ Wormhole/Assets/Scripts/AsteriodRotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteriodRotate : MonoBehaviour
+{
+    public float Xspeed = 200f; //you can change this value in the inspector
+    public float Yspeed = 200f;
+    public float Zspeed = 200f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Time.deltaTime * Xspeed, Time.deltaTime * Yspeed, Time.deltaTime * Zspeed);
+    }
+}
